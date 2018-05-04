e48b9b6ec613b3428449661ee5b528c9ef36b0c8
Jirawat Yuktawathin <yuk.jirawat@gmail.com>
2018-05-05 01:07:27 +0700

    Edit indent from tab to space

--- ALU.vhd
+++ ALU.vhd
@@ -3,29 +3,29 @@ use ieee.std_logic_1164.all;
 use ieee.std_logic_unsigned.all;
 
 entity ALU is
-	port(
-		clk: in std_logic;
-		op: in std_logic_vector(1 downto 0);		-- operation code
-		
-		rs: in std_logic_vector(7 downto 0);		-- source register 1
-		rt: in std_logic_vector(7 downto 0);		-- source register 2
-		
-		rd: out std_logic_vector(7 downto 0)		-- destination register
-	);
+  port(
+    clk : in std_logic;
+    op  : in std_logic_vector(1 downto 0);   -- operation code
+
+    rs  : in std_logic_vector(7 downto 0);   -- source register 1
+    rt  : in std_logic_vector(7 downto 0);   -- source register 2
+
+    rd  : out std_logic_vector(7 downto 0)   -- destination register
+  );
 end ALU;
 
 architecture behavioral of ALU is
 begin
-	process(clk)
-	begin
-		if (op = "00") then			-- AND Gate
-			rd <= rs and rt;
-		elsif (op = "01") then		-- OR Gate
-			rd <= rs or rt;
-		elsif (op = "10") then		-- ADD
-			rd <= rs + rt;
-		elsif (op = "11") then		-- SUB
-			rd <= rs - rt;
-		end if;
-	end process;
+  process(clk)
+  begin
+    if (op = "00") then      -- AND Gate
+      rd <= rs and rt;
+    elsif (op = "01") then   -- OR Gate
+      rd <= rs or rt;
+    elsif (op = "10") then   -- ADD
+      rd <= rs + rt;
+    elsif (op = "11") then   -- SUB
+      rd <= rs - rt;
+    end if;
+  end process;
 end behavioral;
--- Registers.vhd
+++ Registers.vhd
@@ -2,27 +2,26 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity Registers is
-	port(
-		clk		: in std_logic;
-		wr			: in std_logic;									-- write register control
-		
-		rs_addr	: in std_logic_vector(1 downto 0);			-- source register 1 address
-		rt_addr	: in std_logic_vector(1 downto 0);			-- source register 2 address
-		rd_addr	: in std_logic_vector(1 downto 0);			-- destination register address
-		wr_data	: in std_logic_vector(7 downto 0);			-- write data to destination register
-		
-		rs			: out std_logic_vector(7 downto 0);			-- source register 1
-		rt			: out std_logic_vector(7 downto 0)			-- source register 2
-	);
+  port(
+    clk     : in std_logic;
+    wr      : in std_logic;                       -- write register control
+
+    rs_addr : in std_logic_vector(1 downto 0);    -- source register 1 address
+    rt_addr : in std_logic_vector(1 downto 0);    -- source register 2 address
+    rd_addr : in std_logic_vector(1 downto 0);    -- destination register address
+    wr_data : in std_logic_vector(7 downto 0);    -- write data to destination register
+
+    rs      : out std_logic_vector(7 downto 0);   -- source register 1
+    rt      : out std_logic_vector(7 downto 0)    -- source register 2
+  );
 end Registers;
 
 
 architecture behavioral of Registers is
 
 begin
-	process(clk)
-	begin
-		
-	end process;
+  process(clk)
+  begin
+  end process;
 
 end behavioral;
