e10e6be9bdea5966908b189ac274809b0bf7c159
Jirawat Yuktawathin <yuk.jirawat@gmail.com>
2018-05-08 19:38:12 +0700

    Edit register array field

--- Registers.vhd
+++ Registers.vhd
@@ -6,7 +6,6 @@ entity Registers is
   port(
     clk     : in std_logic;
     wr      : in std_logic;                       -- write register control
-    rst     : in std_logic;                       -- reset register value
 
     rs_addr : in std_logic_vector(1 downto 0);    -- source register 1 address
     rt_addr : in std_logic_vector(1 downto 0);    -- source register 2 address
@@ -14,8 +13,7 @@ entity Registers is
     wr_data : in std_logic_vector(7 downto 0);    -- write data to destination register
 
     rs      : out std_logic_vector(7 downto 0);   -- source register 1
-    rt      : out std_logic_vector(7 downto 0);   -- source register 2
-    rd      : out std_logic_vector(7 downto 0)
+    rt      : out std_logic_vector(7 downto 0)    -- source register 2
   );
 end Registers;
 
@@ -23,17 +21,17 @@ end Registers;
 architecture behavioral of Registers is
 
   type registerFile is array(0 to 3) of std_logic_vector(7 downto 0);
-  signal reg: registerFile;
+  signal reg: registerFile := (
+    "00000001",
+    "00000010",
+    "00000011",
+    "00000101"
+  );
 
 begin
-  process(clk, rst)
+  process(clk)
   begin
-    if (rst = '1') then
-      reg(0) <= x"01";
-      reg(1) <= x"10";
-      reg(2) <= x"11";
-      reg(3) <= x"33";
-    elsif rising_edge(clk) then
+    if rising_edge(clk) then
       if (wr = '1') then
         reg(to_integer(unsigned(rd_addr))) <= wr_data;
       end if;
